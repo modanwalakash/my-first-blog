12163457bbc096213f8a8b202ac2ea11dfdffc11
coolakg007 <72658979+coolakg007@users.noreply.github.com>
2022-10-09 16:52:13 +0530

    Update urls.py
    Url.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+""" This file includes all the url in the project and its description 
 urlpatterns=[
     url(r'^$',views.post_list,name='post_list'),
    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name="post_detail"),
